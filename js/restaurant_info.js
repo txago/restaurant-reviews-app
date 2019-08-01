6dc995a130f40ad4138cb73d5de9842423091081
Thiago Menezes <5039518+txago@users.noreply.github.com>
2019-08-01 14:28:35 -0300

    Adjusts list query

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -148,7 +148,7 @@ createReviewHTML = (review) => {
  * Add restaurant name to the breadcrumb navigation menu
  */
 fillBreadcrumb = (restaurant = self.restaurant) => {
-  const breadcrumb = document.getElementById('breadcrumb');
+  const breadcrumb = document.querySelector('#breadcrumb ul');
   const li = document.createElement('li');
   li.innerHTML = restaurant.name;
   breadcrumb.appendChild(li);
